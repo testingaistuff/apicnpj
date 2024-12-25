810213bf2db1824045bb7095a7495a287d74f009
ldgobi <gobi.luciano@gmail.com>
2024-12-25 18:37:11 -0300

    Delete supplier-api/target directory

--- supplier-api/target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO supplier (Nome, Cnpj, NomeContato, EmailContato, TelefoneContato) VALUES 
-('Supplier A', 12345678000190, 'Contact A', 'contactA@example.com', '123456789'),
-('Supplier B', 98765432000101, 'Contact B', 'contactB@example.com', '987654321'),
-('Supplier C', 11222333000111, 'Contact C', 'contactC@example.com', '456789123');
--- supplier-api/target/classes/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE SUPPLIER (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    Nome VARCHAR(255) NOT NULL,
-    Cnpj BIGINT NOT NULL,
-    NomeContato VARCHAR(255),
-    EmailContato VARCHAR(255),
-    TelefoneContato VARCHAR(20)
-);
