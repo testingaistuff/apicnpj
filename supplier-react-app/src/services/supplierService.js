72b186c81a762c090cb10caf79c597f2bfaf0577
ldgobi <gobi.luciano@gmail.com>
2024-12-26 13:54:17 -0300

    Update supplierService.js

--- supplier-react-app/src/services/supplierService.js
+++ supplier-react-app/src/services/supplierService.js
@@ -1,4 +1,4 @@
-const serverUrl = 'http://localhost:8080';
+const serverUrl = 'http://localhost:8081';
 
 export const getAllSuppliers = async () => {
     const response = await fetch(serverUrl+'/api/suppliers');
