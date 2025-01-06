6b5bdcb61ffc45d8b9e2830cb3f2b9e27f3f40aa
ldgobi <gobi.luciano@gmail.com>
2025-01-06 11:46:40 -0300

    exception handling with json response

---
+++ supplier-api/src/main/java/com/example/supplier/ErrorResponse.java
@@ -0,0 +1,5 @@
+package com.example.supplier;
+
+public class ErrorResponse {
+
+}
--- supplier-api/src/main/java/com/example/supplier/GlobalExceptionHandler.java
+++ supplier-api/src/main/java/com/example/supplier/GlobalExceptionHandler.java
@@ -11,19 +11,22 @@ import jakarta.validation.ConstraintViolationException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Validation error: " + ex.getMessage());
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), "Validation error: " + ex.getMessage()));
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse(HttpStatus.BAD_REQUEST.toString(), ex.getMessage()),
+                HttpStatus.BAD_REQUEST);
     }
 
 	@ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
 		e.printStackTrace();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("An error occurred: " + e.getMessage());
+                .body(new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+                        "An error occurred: " + e.getMessage()));
     }
 }
